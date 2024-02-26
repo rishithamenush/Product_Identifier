1329142480233a4c04857611c4aafa4f7791bbaa
rishitha.menush <rishitha.menush@gmail.com>
2024-02-26 20:47:41 +0530

    Update the main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -32,39 +32,43 @@ class _MyHomePageState extends State<MyHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("Super Market Product Identifier"),
+        title: const Center(child: Text("Super Market Product Identifier")),
       ),
-      body: Column(
-        children: [
-          const SizedBox(
-            height: 12,
-          ),
-          Card(
-            elevation: 20,
-            clipBehavior: Clip.hardEdge,
-            child: SizedBox(
-              width: 300,
-              child: SingleChildScrollView(
-                child: Column(
-                  children: [
-                    SizedBox(
-                      height: 18,
-                    ),
-                    Container(
-                      height: 280,
-                      width: 280,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(12),
-                          image: const DecorationImage(
-                              image: AssetImage('assets/capture.png'))),
-                    )
-                  ],
-                ),
+      body: SingleChildScrollView(
+        child: Center(
+          child: Column(
+            children: [
+              const SizedBox(
+                height: 12,
               ),
-            ),
-          )
-        ],
+              Card(
+                elevation: 20,
+                clipBehavior: Clip.hardEdge,
+                child: SizedBox(
+                  width: 300,
+                  child: SingleChildScrollView(
+                    child: Column(
+                      children: [
+                        SizedBox(
+                          height: 18,
+                        ),
+                        Container(
+                          height: 250,
+                          width: 250,
+                          decoration: BoxDecoration(
+                              color: Colors.white,
+                              borderRadius: BorderRadius.circular(12),
+                              image: const DecorationImage(
+                                  image: AssetImage('assets/_capture.png'))),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
       ),
     );
   }
